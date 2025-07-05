ae531eb6e6b61cf4db1264520325295ad24efd2a
Tanvir Islam <mdtanvirislams@gmail.com>
2025-07-05 14:34:29 +1000

    generic models

--- store/models.py
+++ store/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     #sku = models.CharField(max_length=10, primary_key=True)
@@ -12,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotion = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
