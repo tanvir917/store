59950263601697cf376b53fb143d00d352e290ff
tanvirsoniq <tanvir.islam@soniq.com>
2025-07-09 16:58:17 +1000

    generic views

--- store/serializers.py
+++ store/serializers.py
@@ -6,7 +6,7 @@ class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id', 'title', 'featured_product', 'products_count']
-    products_count = serializers.IntegerField()
+    products_count = serializers.IntegerField(read_only=True)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -17,26 +17,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'slug', 'inventory','unit_price', 'price_with_tax', 'collection']
-        #fields = '__all__'  # Use this to include all fields from the model
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-    # description = serializers.CharField()
-    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_price_with_tax')
-    # inventory = serializers.IntegerField()
-    # last_update = serializers.DateTimeField()
-
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset=Collection.objects.all(),
-    #     view_name='collection_detail'
-    # )
-    #collection = CollectionSerializer()
-    #collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
-    #collection = serializers.StringRelatedField()
-    # promotion = serializers.ListField(
-    #     child=serializers.CharField(source='description'),
-    #     allow_empty=True
-    # )
 
     def calculate_price_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.15)
--- store/views.py
+++ store/views.py
@@ -4,23 +4,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.db.models.aggregates import Count
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all().order_by('id')[995:]
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('collection').all().order_by('id')[995:]
+    serializer_class = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 class ProductDetail(APIView):
     def get(self, request, id):
@@ -44,23 +38,15 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(
-            products_count=Count('product'),
-        ).all()
-         #.select_related('featured_product')
-         #.prefetch_related('products')
-         #.order_by('id')
-        serializer = CollectionSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(
+        products_count=Count('product'),
+    ).all()
+    serializer_class = CollectionSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
@@ -82,3 +68,38 @@ def collection_detail(request, pk):
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# @api_view(['GET', 'POST'])
+# def collection_list(request):
+#     if request.method == 'GET':
+#         queryset = Collection.objects.annotate(
+#             products_count=Count('product'),
+#         ).all()
+#          #.select_related('featured_product')
+#          #.prefetch_related('products')
+#          #.order_by('id')
+#         serializer = CollectionSerializer(queryset, many=True, context={'request': request})
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     elif request.method == 'POST':
+#         serializer = CollectionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def get_queryset(self):
+    #     return Product.objects.select_related('collection').all().order_by('id')[995:]
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+
+    # def get(self, request):
+    #     queryset = Product.objects.select_related('collection').all().order_by('id')[995:]
+    #     serializer = ProductSerializer(queryset, many=True, context={'request': request})
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # def post(self, request):
+    #     serializer = ProductSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
