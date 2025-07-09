bb2b39038aeaf911f80055fe683c79f95c9390b4
tanvirsoniq <tanvir.islam@soniq.com>
2025-07-09 16:01:00 +1000

    adding CRUD operations

--- store/views.py
+++ store/views.py
@@ -7,10 +7,10 @@ from .serializers import CollectionSerializer, ProductSerializer
 from rest_framework import status
 
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST']) 
 def product_list(request):
     if request.method == 'GET':
-        queryset = Product.objects.select_related('collection').all()[:5]
+        queryset = Product.objects.select_related('collection').all().order_by('id')[995:]
         serializer = ProductSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
@@ -19,18 +19,49 @@ def product_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view()
+ 
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
-    #Products.objects.get(id=id)/get_object_or_404(Products, id=id)
+     #Products.objects.get(id=id)/get_object_or_404(Products, id=id)
     product = get_object_or_404(Product, pk=id)
-    #convert the product instance to a dictionary
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        if product.orderitems.exists():
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, 
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view()
+@api_view(['GET', 'POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.all().order_by('id')
+        serializer = CollectionSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT'])
 def collection_detail(request, pk):
     collection = get_object_or_404(Collection, pk=pk)
-    serializer = CollectionSerializer(collection)
-    return Response(serializer.data)
-    #return Response('ok')
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(collection, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
